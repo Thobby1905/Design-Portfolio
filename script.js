cbc8bb542aef904cce96b5531af9d93cdbdbc40b
Thobby1905 <ahmedolusesi64@gmail.com>
2025-08-29 11:18:21 +0100

    Added styling and dark mode JS

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-document.getElementById("toggle-theme").addEventListener("click", function () {
-  document.body.classList.toggle("dark-mode");
+// Dark mode toggle
+const toggleBtn = document.getElementById("toggle-theme");
+toggleBtn.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+
+  // change icon between moon and sun
+  if (document.body.classList.contains("dark")) {
+    toggleBtn.textContent = "☀️";
+  } else {
+    toggleBtn.textContent = "🌙";
+  }
 });
